ddca91dc208516be0f82797acad7686aeaff4810
jhay <jhay.antiguo@gmail.com>
2022-01-25 20:42:33 -0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -285,8 +285,10 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                     }
                     else {
                         api.getUserInfo(event.senderID, (err, data) => {
-                            if (err) return console.error("Error: files are"+err+ "\nAnti Unsent By JhayBot");
-                            api.sendMessage("Error: files are"+err+ "\n\nAnti Unsent By JhayBot", event.threadID);
+                            if (err)
+                            {
+                                api.sendMessage("Error: files are"+err+ "\n\nAnti Unsent By JhayBot", event.threadID);
+                            }
                             else {
                                 api.sendMessage(data[event.senderID]['name'] + " unsent this: \n" + msgs[event.messageID] + "\n\nAnti Unsent By JhayBot", event.threadID);
                             }
