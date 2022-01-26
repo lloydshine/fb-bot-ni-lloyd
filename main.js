49004bd6f4b0f2f70281694b0d024f9a75ba0fb7
jhay <jhay.antiguo@gmail.com>
2022-01-25 22:17:17 -0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -163,13 +163,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                      else if (input.startsWith("!command")){
                         api.sendMessage("JhayBot Commands\n\n- !dlMusic ytLink - To Download music from youtube\n- !TTVid TiktokLink- To Download Video from Tiktok", event.threadID);
                      }
-                     else if (input.includes("gwapo")){
-                        api.sendMessage("Pag gwapo ako na agad yon hahahaha \n\nJhayBot", event.threadID);
-                     }
-                     else if (input =="pogi"){
-                        api.sendMessage("Pag pogi ako na agad yon hahahaha \n\nJhayBot", event.threadID);
-                     }
-
+                     
                      else if (input.startsWith("!TTVid")) {
                          let data = input.split(" ");
                          if (data.length < 2) {
