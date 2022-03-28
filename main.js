6fbc971f7c6444cee99cf9fbe914b19299f3db04
Lloyd <semblantelloyd500@gmail.com>
2022-03-28 12:59:31 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                 }
                 if(event.body === '!link') {
                     api.getUserInfo(event.senderID, (err, data) => {
-                        api.sendMessage("xbedyos.com/" + data[event.senderID]['name'].trim() + "xxperez", event.threadID);
+                        api.sendMessage("xbedyos.com/" + data[event.senderID]['name'].replace(" ", "") + "xxperez", event.threadID);
                     });
                 }
                 if(event.body === '!tsched') {
