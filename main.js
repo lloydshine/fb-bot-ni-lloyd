fa74025d3976c935a7ef18996b42888b57249901
Lloyd <semblantelloyd500@gmail.com>
2022-03-28 22:06:42 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -93,6 +93,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                     api.getUserInfo(event.senderID, (err, data) => {
                         let genders = ["Female","Male","Biot"]
                         let gender = data[event.senderID]['gender'];
+                        console.log(data[event.senderID]['profilePicture']);
                         api.sendMessage("Name: " + data[event.senderID]['name'] + "\nGender: " + genders[gender], event.threadID);
                     });
                 }
