51d5965397e2ea382b94e3bd761b4650e952d2cb
Lloyd <semblantelloyd500@gmail.com>
2022-03-28 21:55:46 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                 }
                 if(event.body === '!myinfo') {
                     api.getUserInfo(event.senderID, (err, data) => {
-                        api.sendMessage("Name: " + data[event.senderID]['name'] + "\nGender: " + data[event.senderID]['gender'] + data[event.senderID]['profilePicture'], event.threadID);
+                        api.sendMessage("Name: " + data[event.senderID]['name'] + "\nGender: " + data[event.senderID]['gender'] + "\n" + data[event.senderID]['profilePicture'], event.threadID);
                     });
                 }
                 if(event.body === '!sched') {
