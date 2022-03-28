6b116b92fc23f3585e5e283514b209487d19de6d
Lloyd <semblantelloyd500@gmail.com>
2022-03-28 13:58:23 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                     api.sendMessage("Hail Jhon Hesper the Alpha Male!", event.threadID);
                 }
                 if(event.body === '!help') {
-                    api.sendMessage("Commands:\n!link - Get online class link.\n!tsched - Get current day schedule.\n!perezgae - To bully Perez!\n!perezAlphamale - Perez the Alpha Male!", event.threadID);
+                    api.sendMessage("Commands:\n!link - Get online class link.\n!tsched - Get current day schedule.\n!spin - To spin a random number from 1 - 50.\n!perezgae - To bully Perez!\n!perezAlphamale - Perez the Alpha Male!", event.threadID);
                 }
                 if(event.body === '!link') {
                     api.getUserInfo(event.senderID, (err, data) => {
