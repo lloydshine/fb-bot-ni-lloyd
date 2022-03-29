3747f6ed8e93fbcfc5fa5f5995a805bade0c1efe
Lloyd <semblantelloyd500@gmail.com>
2022-03-29 08:23:22 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -33,16 +33,14 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                 break;
             case "message":
                 if(event.body.includes("!search")) {
-                    api.getUserInfo(event.senderID, (err, data) => {
-                        let s = event.body.split(" ");
-                        if (s[0] == "!search") {
-                            let searcht = s[1]
-                            const encoded = encodeURI(searcht);
-                            api.sendMessage({
-                                "url": `https://www.google.com/search?q=${encoded}`
-                            }, threadId);
-                        }
-                    });
+                    let s = event.body.split(" ");
+                    if (s[0] == "!search") {
+                        let searcht = s[1]
+                        const encoded = encodeURI(searcht);
+                        api.sendMessage({
+                            "url": `https://www.google.com/search?q=${encoded}`
+                        }, event.threadId);
+                    }
                 }
                 if(event.body === "!spin") {
                     api.getUserInfo(event.senderID, (err, data) => {
