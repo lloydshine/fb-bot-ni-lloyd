59b02ae8427be884f952b5bbc5cd7f1a82315a59
Lloyd <semblantelloyd500@gmail.com>
2022-04-11 14:43:34 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -57,10 +57,10 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                          api.setMessageReaction("❓", event.messageID, (err) => {
                          }, true);
                      } */
-                    let msgid = event.messageID
-                    let input = event.body;
-                    msgs[msgid] = input;
-                    break;
+                    //let msgid = event.messageID
+                    //let input = event.body;
+                    //msgs[msgid] = input;
+                    //break;
                 case "message":
                     if (gcblock.includes(event.threadID)) {
                         //console.log(event.senderID)
@@ -304,127 +304,127 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
                             api.sendMessage(">"+todaymsg + "\nIT Class Schedule: \nNSTP 02 - Bagsak nata!", event.threadID);
                         }
                     }
-                    if (event.attachments.length != 0) {
-                        if (event.attachments[0].type == "photo") {
-                            msgs[event.messageID] = ['img', event.attachments[0].url]
-                        }
-                        else if (event.attachments[0].type == "video") {
-                            msgs[event.messageID] = ['vid', event.attachments[0].url]
-                        }
-                        else if (event.attachments[0].type == "audio") {
-                            msgs[event.messageID] = ['vm', event.attachments[0].url]
-                        }
-                        else if (event.attachments[0].type == "animated_image") {
-                            msgs[event.messageID] = ['gif', event.attachments[0].url]
-                        }
-                        else if (event.attachments[0].type == "sticker") {
-                            msgs[event.messageID] = ['sticker', event.attachments[0].url]
-                        }
-                        else if (event.attachments[0].type == "file") {
-                            msgs[event.messageID] = ['file', event.attachments[0].url]
-                        }
-
-                    } else {
-                        msgs[event.messageID] = event.body
-                    }
+                    //if (event.attachments.length != 0) {
+                    //    if (event.attachments[0].type == "photo") {
+                    //        msgs[event.messageID] = ['img', event.attachments[0].url]
+                    //    }
+                    //    else if (event.attachments[0].type == "video") {
+                    //        msgs[event.messageID] = ['vid', event.attachments[0].url]
+                    //    }
+                    //    else if (event.attachments[0].type == "audio") {
+                    //        msgs[event.messageID] = ['vm', event.attachments[0].url]
+                    //    }
+                    //    else if (event.attachments[0].type == "animated_image") {
+                    //        msgs[event.messageID] = ['gif', event.attachments[0].url]
+                    //    }
+                    //    else if (event.attachments[0].type == "sticker") {
+                    //        msgs[event.messageID] = ['sticker', event.attachments[0].url]
+                    //    }
+                    //    else if (event.attachments[0].type == "file") {
+                    //        msgs[event.messageID] = ['file', event.attachments[0].url]
+                    //    }
+//
+                    //} else {
+                    //    msgs[event.messageID] = event.body
+                    //}
 
                     break;
-                case "message_unsend":
-                    if (gcblock.includes(event.threadID)) {
-                        break;
-                    }
-                    if (!vips.includes(event.senderID)) {
-                        let d = msgs[event.messageID];
-                        if (typeof (d) == "object") {
-                            api.getUserInfo(event.senderID, (err, data) => {
-                                if (err) return console.error(err);
-                                else {
-                                    if (d[0] == "img") {
-                                        var file = fs.createWriteStream("photo.jpg");
-                                        var gifRequest = http.get(d[1], function (gifResponse) {
-                                            gifResponse.pipe(file);
-                                            file.on('finish', function () {
-                                                console.log('finished downloading photo..')
-                                                var message = {
-                                                    body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this photo: \n",
-                                                    attachment: fs.createReadStream(__dirname + '/photo.jpg')
-                                                }
-                                                api.sendMessage(message, event.threadID);
-                                            });
-                                        });
-                                    }
-                                    else if (d[0] == "vid") {
-                                        var file = fs.createWriteStream("video.mp4");
-                                        var gifRequest = http.get(d[1], function (gifResponse) {
-                                            gifResponse.pipe(file);
-                                            file.on('finish', function () {
-                                                console.log('finished downloading video..')
-                                                var message = {
-                                                    body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this video: \n",
-                                                    attachment: fs.createReadStream(__dirname + '/video.mp4')
-                                                }
-                                                api.sendMessage(message, event.threadID);
-                                            });
-                                        });
-                                    }// GIF unsent test
-                                    else if (d[0] == "gif") {
-                                        var file = fs.createWriteStream("animated_image.gif");
-                                        var gifRequest = http.get(d[1], function (gifResponse) {
-                                            gifResponse.pipe(file);
-                                            file.on('finish', function () {
-                                                console.log('finished downloading gif..')
-                                                var message = {
-                                                    body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this GIF: \n",
-                                                    attachment: fs.createReadStream(__dirname + '/animated_image.gif')
-                                                }
-                                                api.sendMessage(message, event.threadID);
-                                            });
-                                        });
-                                    }
-                                
-                                    else if (d[0] == "sticker") {
-                                        var file = fs.createWriteStream("sticker.png");
-                                        var gifRequest = http.get(d[1], function (gifResponse) {
-                                            gifResponse.pipe(file);
-                                            file.on('finish', function () {
-                                                console.log('finished downloading gif..')
-                                                var message = {
-                                                    body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this Sticker: \n",
-                                                    attachment: fs.createReadStream(__dirname + '/sticker.png')
-                                                }
-                                                api.sendMessage(message, event.threadID);
-                                            });
-                                        });
-                                    }
-
-                                    else if (d[0] == "vm") {
-                                        var file = fs.createWriteStream("vm.mp3");
-                                        var gifRequest = http.get(d[1], function (gifResponse) {
-                                            gifResponse.pipe(file);
-                                            file.on('finish', function () {
-                                                console.log('finished downloading audio..')
-                                                var message = {
-                                                    body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this audio: \n",
-                                                    attachment: fs.createReadStream(__dirname + '/vm.mp3')
-                                                }
-                                                api.sendMessage(message, event.threadID);
-                                            });
-                                        });
-                                    }
-                                }
-                            });
-                        }
-                        else {
-                            api.getUserInfo(event.senderID, (err, data) => {
-                                if (err) return console.error("Error: files are"+err+ "\nJhayBot");
-
-                                else {
-                                    api.sendMessage("[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this: \n" + msgs[event.messageID], event.threadID);
-                                }
-                            });
-                        }
-                        break;
-                    }
+                //case "message_unsend":
+                //    if (gcblock.includes(event.threadID)) {
+                //        break;
+                //    }
+                    //if (!vips.includes(event.senderID)) {
+                    //    let d = msgs[event.messageID];
+                    //    if (typeof (d) == "object") {
+                    //        api.getUserInfo(event.senderID, (err, data) => {
+                    //            if (err) return console.error(err);
+                    //            else {
+                    //                if (d[0] == "img") {
+                    //                    var file = fs.createWriteStream("photo.jpg");
+                    //                    var gifRequest = http.get(d[1], function (gifResponse) {
+                    //                        gifResponse.pipe(file);
+                    //                        file.on('finish', function () {
+                    //                            console.log('finished downloading photo..')
+                    //                            var message = {
+                    //                                body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this photo: \n",
+                    //                                attachment: fs.createReadStream(__dirname + '/photo.jpg')
+                    //                            }
+                    //                            api.sendMessage(message, event.threadID);
+                    //                        });
+                    //                    });
+                    //                }
+                    //                else if (d[0] == "vid") {
+                    //                    var file = fs.createWriteStream("video.mp4");
+                    //                    var gifRequest = http.get(d[1], function (gifResponse) {
+                    //                        gifResponse.pipe(file);
+                    //                        file.on('finish', function () {
+                    //                            console.log('finished downloading video..')
+                    //                            var message = {
+                    //                                body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this video: \n",
+                    //                                attachment: fs.createReadStream(__dirname + '/video.mp4')
+                    //                            }
+                    //                            api.sendMessage(message, event.threadID);
+                    //                        });
+                    //                    });
+                    //                }// GIF unsent test
+                    //                else if (d[0] == "gif") {
+                    //                    var file = fs.createWriteStream("animated_image.gif");
+                    //                    var gifRequest = http.get(d[1], function (gifResponse) {
+                    //                        gifResponse.pipe(file);
+                    //                        file.on('finish', function () {
+                    //                            console.log('finished downloading gif..')
+                    //                            var message = {
+                    //                                body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this GIF: \n",
+                    //                                attachment: fs.createReadStream(__dirname + '/animated_image.gif')
+                    //                            }
+                    //                            api.sendMessage(message, event.threadID);
+                    //                        });
+                    //                    });
+                    //                }
+                    //            
+                    //                else if (d[0] == "sticker") {
+                    //                    var file = fs.createWriteStream("sticker.png");
+                    //                    var gifRequest = http.get(d[1], function (gifResponse) {
+                    //                        gifResponse.pipe(file);
+                    //                        file.on('finish', function () {
+                    //                            console.log('finished downloading gif..')
+                    //                            var message = {
+                    //                                body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this Sticker: \n",
+                    //                                attachment: fs.createReadStream(__dirname + '/sticker.png')
+                    //                            }
+                    //                            api.sendMessage(message, event.threadID);
+                    //                        });
+                    //                    });
+                    //                }
+//
+                    //                else if (d[0] == "vm") {
+                    //                    var file = fs.createWriteStream("vm.mp3");
+                    //                    var gifRequest = http.get(d[1], function (gifResponse) {
+                    //                        gifResponse.pipe(file);
+                    //                        file.on('finish', function () {
+                    //                            console.log('finished downloading audio..')
+                    //                            var message = {
+                    //                                body: "[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this audio: \n",
+                    //                                attachment: fs.createReadStream(__dirname + '/vm.mp3')
+                    //                            }
+                    //                            api.sendMessage(message, event.threadID);
+                    //                        });
+                    //                    });
+                    //                }
+                    //            }
+                    //        });
+                    //    }
+                    //    else {
+                    //        api.getUserInfo(event.senderID, (err, data) => {
+                    //            if (err) return console.error("Error: files are"+err+ "\nJhayBot");
+//
+                    //            else {
+                    //                api.sendMessage("[JhayBot] Anti Unsent:\n" + data[event.senderID]['name'] + " unsent this: \n" + msgs[event.messageID], event.threadID);
+                    //            }
+                    //        });
+                    //    }
+                    //    break;
+                    //}
             }
         }
     });
