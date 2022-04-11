f8d0a06bbed4f487586a9aac44eedf3f531130a2
Lloyd <semblantelloyd500@gmail.com>
2022-04-12 07:27:27 +0800

    auto deploy by salvador

--- main.js
+++ main.js
@@ -16,9 +16,14 @@ let vips = ['100016092066464','100009687019306','100008672340619']; //TO MAKE YO
 login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
     if (err) return console.error(err);
     api.setOptions({ listenEvents: true });
+    api.listen((err, message) => {
+        if(err) return console.error(err);
+
+        // Marks messages as read immediately after they're received
+        api.markAsRead(message.threadID);
+    });
     var listenEmitter = api.listen((err, event) => {
         if (gc.includes(event.threadID) || gcblock.includes(event.threadID)) {
-            api.markAsRead(event.threadID);
             if (err) return console.error(err);
             switch (event.type) {
                 case "event":
