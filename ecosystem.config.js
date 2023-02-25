1c92060f1e2de5bb419c18c4f23f9a66fbe42294
lloydshinee <semblantelloyd500@gmail.com>
2023-02-26 07:26:46 +0800

    New

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
       name: "Process",
       script: "./main.js",
       interpreter_args: "--max-old-space-size=4096",
-      cron_restart: "20 7 * * *",
+      cron_restart: "30 7 * * * Asia/Manila",
     },
     {
       name: "Scheduler",
       script: "./scheduler.js",
-      cron_restart: "0 0 8 * * *", // Restarts the app at 8:00 AM every day
-      cron_stop: "0 0 17 * * *", // Stops the app at 5:00 PM every day
+      cron_restart: "0 0 8 * * * Asia/Manila", // Restarts the app at 8:00 AM every day
+      cron_stop: "0 0 17 * * * Asia/Manila", // Stops the app at 5:00 PM every day
     },
   ],
 };
