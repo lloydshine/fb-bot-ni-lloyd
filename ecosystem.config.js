e2300edaf221ba7ce620d1575297e4e934abd6a1
lloydshinee <semblantelloyd500@gmail.com>
2023-02-25 14:55:55 +0800

    Change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  apps : [{
-    name   : "Process",
-    script : "./main.js",
-    args: "--max-old-space-size=4096",
-  }]
-}
+  apps: [
+    {
+      name: "Process",
+      script: "./main.js",
+      args: "--max-old-space-size=4096",
+    },
+    {
+      name: "Scheduler",
+      script: "./scheduler.js",
+      cron_restart: "0 8-16 * * *", // Runs the app from 8:00 AM to 5:00 PM every day
+      cron_stop: "0 17 * * *", // Stops the app at 5:00 PM every day
+    },
+  ],
+};
