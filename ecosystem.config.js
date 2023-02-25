b06c07ae245b1d5da0d38fd8f0d22f14ccf16f0c
lloydshinee <semblantelloyd500@gmail.com>
2023-02-25 19:14:00 +0800

    change timezone

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
     {
       name: "Scheduler",
       script: "./scheduler.js",
-      cron_restart: "0 8-16 * * *", // Runs the app from 8:00 AM to 5:00 PM every day
-      cron_stop: "0 17 * * *", // Stops the app at 5:00 PM every day
+      cron_restart: "0 0 8-16 * * * Asia/Manila", // Runs the app from 8:00 AM to 5:00 PM every day in Asia/Manila timezone
+      cron_stop: "0 0 17 * * * Asia/Manila", // Stops the app at 5:00 PM every day in Asia/Manila timezone
     },
   ],
 };
