d841a19ade947cdbad558bc43f08f7e9226e03fa
lloydshinee <semblantelloyd500@gmail.com>
2023-02-27 00:11:36 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "rb",
       script: "./main.js",
-      cron_restart: "0 0,12 * * *", // Restarts the app every day at 12:00 AM and 12:00 PM
+      cron_restart: "15 0,12 * * *", // Restarts the app every day at 12:00 AM and 12:00 PM
       env: {
         TZ: "Asia/Manila" // Set the timezone to Asia/Manila
       }
