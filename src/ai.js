fd21fc89bbf09f483a2f3fab757257a3a3e76d0e
lloydshinee <semblantelloyd500@gmail.com>
2023-03-01 13:43:51 +0800

    revert

--- src/ai.js
+++ src/ai.js
@@ -21,11 +21,7 @@ async function ai(event, command, api) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: command[1],
-      temperature: 0.5,
-      max_tokens: 60,
-      top_p: 0.3,
-      frequency_penalty: 0.5,
-      presence_penalty: 0.0,
+      max_tokens: 1000,
     });
 
     api.sendMessage(
