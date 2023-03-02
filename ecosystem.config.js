24c064c578c19458ce8b0befbe8c2514b18940b1
lloydshinee <semblantelloyd500@gmail.com>
2023-03-02 09:14:08 +0800

    new args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "rb",
-      script: "./main.js",
-      cron_restart: "15 0,12 * * *", // Restarts the app every day at 12:00 AM and 12:00 PM
-      env: {
-        TZ: "Asia/Manila" // Set the timezone to Asia/Manila
+      "name": "rb",
+      "script": "./main.js",
+      "cron_restart": "15 0,12 * * *",
+      "env": {
+        "TZ": "Asia/Manila"
       },
-      args: ["--max-old-space-size=2048"] // Increase heap size to 2048 MB
-    }
+      "interpreter_args": ["--max-old-space-size=4096"]
+    }    
   ]
 }
