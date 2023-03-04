43744e719e60b3a5f7ce0f2382c3528cb51317b7
lloydshinee <semblantelloyd500@gmail.com>
2023-03-04 18:40:54 +0800

    fix

--- main.js
+++ main.js
@@ -19,7 +19,12 @@ const gc = [
   "5896664363701089",
   "5030346047032431",
 ];
-const vips = ["100085524705916", "100008672340619", "100009403889511","100001439903602"];
+const vips = [
+  "100085524705916",
+  "100008672340619",
+  "100009403889511",
+  "100001439903602",
+];
 
 login(
   { appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")) },
@@ -79,96 +84,106 @@ login(
       });
     });
 
+    const event_types = ["event","log:subscribe","log:unsubscribe","message_reply","message"];
+
     const listenEmitter = api.listen(async (err, event) => {
       if (!gc.includes(event.threadID) && !gcblock.includes(event.threadID)) {
         return;
       }
       if (err) return console.error(err);
-
+      if(!event_types.includes(event.type)) {
+        return;
+      }
       switch (event.type) {
         case "event":
-          const data = await api.getThreadInfo(event.threadID);
-          if (event.logMessageType === "log:subscribe") {
-            joined(event, data, api);
-          } else if (event.logMessageType === "log:unsubscribe") {
-            left(event, api);
+          if(!event_types.includes(event.logMessageType)) {
+            return;
           }
+          switch (event.logMessageType) {
+            case "log:subscribe":
+              const data = await api.getThreadInfo(event.threadID);
+              joined(event, data, api);
+              break;
+            case "log:unsubscribe":
+              left(event, api);
+              break;
+          }          
           break;
 
         case "message_reply":
         case "message":
-          if (event.body.startsWith("!")) {
-            const command = event.body.split(/(?<=^\S+)\s/);
-            const data = await api.getUserInfo(event.senderID);
-            switch (command[0].toLowerCase()) {
-              case "!reminders":
-                if (!vips.includes(event.senderID)) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                reminders(event, command, api);
-                break;
-              case "!remind":
-                if (!vips.includes(event.senderID)) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                remind(event, command, api);
-                break;
-              case "!imagine":
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                imagine(event, command, api);
-                break;
-              case "!ai":
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                ai(event, command, api);
-                break;
-              case "!nick":
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                nick(event, data, command, api);
-                break;
-              case "!sched":
-                if (event.threadID != "3895005423936924") {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                } else {
-                  sched(event, command[1], api);
-                }
-                break;
-              case "!pin":
-                if(!command[1]) {
-                  api.sendMessage("?", event.threadID, event.messageID);
-                  return;
-                }
-                pin(command[1].split(/(?<=^\S+)\s/), event, api);
-                break;
-              default:
-                api.sendMessage(
-                  "Taka raman kag yawit uy",
-                  event.threadID,
-                  event.messageID
-                );
-                break;
-            }
+          if (!event.body.startsWith("!")) {
+            return;
+          }
+          const command = event.body.split(/(?<=^\S+)\s/);
+          const data = await api.getUserInfo(event.senderID);
+          switch (command[0].toLowerCase()) {
+            case "!reminders":
+              if (!vips.includes(event.senderID)) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              reminders(event, command, api);
+              break;
+            case "!remind":
+              if (!vips.includes(event.senderID)) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              remind(event, command, api);
+              break;
+            case "!imagine":
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              imagine(event, command, api);
+              break;
+            case "!ai":
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              ai(event, command, api);
+              break;
+            case "!nick":
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              nick(event, data, command, api);
+              break;
+            case "!sched":
+              if (event.threadID != "3895005423936924") {
+                api.sendMessage("?", event.threadID, event.messageID);
+              } else {
+                sched(event, command[1], api);
+              }
+              break;
+            case "!pin":
+              if (!command[1]) {
+                api.sendMessage("?", event.threadID, event.messageID);
+                return;
+              }
+              pin(command[1].split(/(?<=^\S+)\s/), event, api);
+              break;
+            default:
+              api.sendMessage(
+                "Taka raman kag yawit uy",
+                event.threadID,
+                event.messageID
+              );
+              break;
           }
           break;
-
         default:
           break;
       }
