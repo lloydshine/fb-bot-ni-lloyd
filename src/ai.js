cce707a56f527cacd258889af5360e55ade32257
lloydshinee <semblantelloyd500@gmail.com>
2023-03-14 13:56:20 +0800

    RERE

--- src/ai.js
+++ src/ai.js
@@ -15,7 +15,7 @@ async function ai(event, command, api) {
       role: "assistant",
       content: `Hello ${
         data[event.senderID]["name"]
-      }, I am Daddy Ken, and I am the father of all!`,
+      }, I am Daddy Kent, and I am the father of all!`,
     },
   ];
   userMessages.push({ role: "user", content: `${command}` }); // Add the new command to the user's messages
