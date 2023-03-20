ef03d54e84bf8433d5af82a3405b0c76778b2231
lloydshinee <semblantelloyd500@gmail.com>
2023-03-20 19:57:12 +0800

    fix

--- src/ai.js
+++ src/ai.js
@@ -5,7 +5,9 @@ const configuration = new Configuration({
   apiKey: process.env.openAI,
 });
 const openai = new OpenAIApi(configuration);
-const messages = {};
+
+// Object to store messages for each threadID
+const messagesByThreadID = {};
 
 function ai(event, command, api) {
   // Add the new command to the user's messages
@@ -13,28 +15,34 @@ function ai(event, command, api) {
     const data = await api.getUserInfo(event.senderID);
     const thread = await api.getThreadInfo(event.threadID);
     const userID = event.senderID;
-    const userMessages = messages[userID] || [
-      {
-        role: "system",
-        content: `You are an AI in a groupchat called ${
-          thread.threadName
-        }, You are programmed to engage in conversations, provide human-like responses. Also keep the response short`,
-      },
-      {
-        role: "assistant",
-        content: `Hello ${
-          data[event.senderID]["name"]
-        }, I am ChatGPT an expert AI, created by Lloyd.`,
-      },
-    ];
+
+    // Create a new messages object for this threadID if it doesn't already exist
+    if (!messagesByThreadID[event.threadID]) {
+      messagesByThreadID[event.threadID] = {};
+    }
+
+    // Get the user's messages for this threadID
+    const userMessages =
+      messagesByThreadID[event.threadID][userID] ||
+      [        {          role: "system",          content: `You are an AI in a groupchat called ${            thread.threadName          }, You are programmed to engage in conversations, provide human-like responses. Also keep the response short`,        },        {          role: "assistant",          content: `Hello ${            data[event.senderID]["name"]
+          }, I am ChatGPT an expert AI, created by Lloyd.`,
+        },
+      ];
+
+    // Add the user's new command to their messages
     userMessages.push({ role: "user", content: `${command}` });
+
     if (err) return console.log(err);
     try {
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: userMessages,
       });
+
+      // Add the AI's response to the user's messages
       userMessages.push(completion.data.choices[0].message);
+
+      // Send the AI's response to the thread
       api.sendMessage(
         completion.data.choices[0].message.content,
         event.threadID,
@@ -46,17 +54,25 @@ function ai(event, command, api) {
         event.threadID,
         event.messageID
       );
+
+      // Add an error message to the user's messages
       userMessages.push({
         role: "assistant",
         content: "Something happened, please try again.",
       });
     }
+
+    // Stop typing indicator
     stopTyping();
-    messages[userID] = userMessages;
-    clearTimeout(messages[userID]?.timer);
-    messages[userID].timer = setTimeout(() => {
+
+    // Save the user's messages for this threadID
+    messagesByThreadID[event.threadID][userID] = userMessages;
+
+    // Set a timer to clear the user's messages after 2 minutes of inactivity
+    clearTimeout(messagesByThreadID[event.threadID][userID]?.timer);
+    messagesByThreadID[event.threadID][userID].timer = setTimeout(() => {
       console.log("Cleared!");
-      delete messages[userID];
+      delete messagesByThreadID[event.threadID][userID];
     }, 2 * 60 * 1000);
   });
 }
