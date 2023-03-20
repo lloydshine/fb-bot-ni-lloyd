4847a46ad21fd3ef459ce22a4b13017b2026237e
lloydshinee <semblantelloyd500@gmail.com>
2023-03-21 00:49:48 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,6 @@ module.exports = {
       "name": "rb",
       "script": "./main.js",
       "cron_restart": "0 */3 * * *",
-      "env": {
-        "TZ": "Asia/Manila"
-      },
       "interpreter_args": ["--max-old-space-size=4096"],
       "restart_delay": "10000",
       "max_memory_restart": "4G"
--- src/ai.js
+++ src/ai.js
@@ -9,39 +9,34 @@ const openai = new OpenAIApi(configuration);
 // Object to store messages for each threadID
 const messagesByThreadID = {};
 
-function ai(event, command, api) {
+async function ai(event, command, api) {
   // Add the new command to the user's messages
   const stopTyping = api.sendTypingIndicator(event.threadID, async (err) => {
     const data = await api.getUserInfo(event.senderID);
-    const thread = await api.getThreadInfo(event.threadID);
     const userID = event.senderID;
-
     // Create a new messages object for this threadID if it doesn't already exist
     if (!messagesByThreadID[event.threadID]) {
       messagesByThreadID[event.threadID] = {};
     }
-
     // Get the user's messages for this threadID
     const userMessages =
       messagesByThreadID[event.threadID][userID] ||
-      [        {          role: "system",          content: `You are an AI in a groupchat called ${            thread.threadName          }, You are programmed to engage in conversations, provide human-like responses. Also keep the response short`,        },        {          role: "assistant",          content: `Hello ${            data[event.senderID]["name"]
-          }, I am ChatGPT an expert AI, created by Lloyd.`,
+      [              
+        {          
+          role: "assistant",          
+          content: `Hello ${data[event.senderID]["name"]}, I am ChatGPT an expert AI, created by Lloyd.`,
         },
       ];
-
     // Add the user's new command to their messages
     userMessages.push({ role: "user", content: `${command}` });
-
     if (err) return console.log(err);
     try {
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: userMessages,
       });
-
       // Add the AI's response to the user's messages
       userMessages.push(completion.data.choices[0].message);
-
       // Send the AI's response to the thread
       api.sendMessage(
         completion.data.choices[0].message.content,
